c812587ea176a5332b408c7f1ff4189ce0b502ee
Usuario <usuario@loquesea.com>
2026-04-17 17:49:31 +0200

    Formularios II

--- reactapp/src/epigrafe9/01FormularioTarea.js
+++ reactapp/src/epigrafe9/01FormularioTarea.js
@@ -1,15 +1,17 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 
 function FormularioTarea({ tareas, setTareas }) {
 
   const [tarea, setTarea] = useState({});
+  const [creandoTarea, setCreandoTarea] = useState(false);
+
+  const focoTitulo = useRef(null);
 
   useEffect( () => {
     // Generar el ID automático
-    const inicio = 0;
     let elMayor = 0;
     if( tareas.length > 0 )
-      elMayor = tareas.reduce( (inicio, tarea) => tarea.id > inicio ? tarea.id : inicio)
+      elMayor = tareas.reduce( (mayor, tarea) => tarea.id > mayor ? tarea.id : mayor, 0);
 
     // Poner la fecha en el formato aceptado por el navegador
     const ahora = new Date();
@@ -17,20 +19,26 @@ function FormularioTarea({ tareas, setTareas }) {
     const ahoraLocal = new Date( ahora - offSet);
     const fecha = ahoraLocal.toISOString().slice(0,16);
 
+    // Todavía no ha pulsado en Crear Tarea
+    setCreandoTarea(false);
+
     // Establecer los valores por defecto de la tarea
     setTarea({id: elMayor + 1, 
               titulo: '', 
               categoria:'', 
-              prioridad:'b', 
+              prioridad:'', 
               remunerado: true,
               fecha: fecha,
-              descripcion: '' });         
+              descripcion: '' }); 
+              
+    focoTitulo.current.focus();
+      
   }, [tareas]);
 
 
   function eventoOnChange( evt ) {
     const clave = evt.target.name;
-    const valor = evt.target.type === 'checkbox' ? evt.target.checked : evt.target.value;
+    let valor = evt.target.type === 'checkbox' ? evt.target.checked : evt.target.value;
 
     setTarea( {...tarea, [clave]:valor} );
 
@@ -39,13 +47,23 @@ function FormularioTarea({ tareas, setTareas }) {
   function envioForm( evt ) {
     evt.preventDefault();
 
+    setCreandoTarea(true);
+    // Campos obligatorios: título, categoría, prioridad, fecha
+    if( tarea.titulo.trim() === '' || tarea.categoria.trim() === '' || tarea.prioridad.trim() === '' || tarea.fecha.trim() === '' ) {
+      alert('Revise el formulario. Hay campos obligatorios sin rellenar');
+      return;
+    } 
+
     // Controlo la tarea
     const encontrado = ['b','m','a','u'].find( (p) => p === tarea.prioridad);
-    if( !encontrado )
+    if( !encontrado ) {
       alert("La prioridad no es válida");
+      return;
+    }
     
     // La tarea es correcta. Se añade al array de tareas
-    setTareas( [...tareas, tarea]);
+    const tareaNueva = { ...tarea, fecha: new Date(tarea.fecha) }
+    setTareas( [...tareas, tareaNueva]);
 
   }
 
@@ -53,18 +71,20 @@ function FormularioTarea({ tareas, setTareas }) {
     <form onSubmit={envioForm}>
       <h3>Agregar una nueva tarea</h3>
       <div>
-        <label for='id'>Id</label>
+        <label htmlFor='id'>Id</label>
         <span>{tarea.id}</span>
       </div>
 
       <div>
-        <label for='titulo'>Título</label> 
-        <input type='text' name='titulo' id='titulo' size='40'
-          value={tarea.titulo} onChange={eventoOnChange}/>
+        <label htmlFor='titulo'>Título *</label> 
+        <input ref={focoTitulo} type='text' name='titulo' id='titulo' size='40'
+          value={tarea.titulo} onChange={eventoOnChange} 
+          className={creandoTarea && tarea.titulo.trim() === '' ? 'input_error' : 'input_ok'}/> &nbsp;
+        {creandoTarea && tarea.titulo.trim() === '' && <span className='error'>El título es obligatorio</span>}
       </div>
 
       <div>
-        <label for='categoria'>Categoría</label>
+        <label htmlFor='categoria'>Categoría *</label>
         <select name='categoria' id='categoria' size='1' 
           value={tarea.categoria} onChange={eventoOnChange}>
           <option value=''>Despliega la lista y elige una</option>
@@ -73,37 +93,38 @@ function FormularioTarea({ tareas, setTareas }) {
           <option value='te'>Tareas de la escuela</option>
           <option value='oc'>Ocio</option>
           <option value='md'>Mantenimiento digital</option>
-        </select>
+        </select> &nbsp;
+        {creandoTarea && tarea.categoria.trim() === '' && <span className='error'>Tiene que elegir una categoría</span>}
       </div>
 
       <div>
-        <label for='prioridad'>Prioridad</label>
-        
-          <input type='radio' name='prioridad' value='b' checked={tarea.prioridad === 'b'} 
-            onChange={eventoOnChange}/>Baja&nbsp;
-          <input type='radio' name='prioridad' value='m' checked={tarea.prioridad === 'm'}
-            onChange={eventoOnChange}/>Media&nbsp;
-          <input type='radio' name='prioridad' value='a' checked={tarea.prioridad === 'a'} 
-            onChange={eventoOnChange}/>Alta&nbsp;
-          <input type='radio' name='prioridad' value='u' checked={tarea.prioridad === 'u'}
-            onChange={eventoOnChange}/>Urgente&nbsp;
-        
+        <label htmlFor='prioridad'>Prioridad *</label>        
+        <input type='radio' name='prioridad' value='b' checked={tarea.prioridad === 'b'} 
+          onChange={eventoOnChange}/>Baja&nbsp;
+        <input type='radio' name='prioridad' value='m' checked={tarea.prioridad === 'm'}
+          onChange={eventoOnChange}/>Media&nbsp;
+        <input type='radio' name='prioridad' value='a' checked={tarea.prioridad === 'a'} 
+          onChange={eventoOnChange}/>Alta&nbsp;
+        <input type='radio' name='prioridad' value='u' checked={tarea.prioridad === 'u'}
+          onChange={eventoOnChange}/>Urgente&nbsp;
+        {creandoTarea && tarea.prioridad.trim() === '' && <span className='error'>Hay que elegir una prioridad</span> }
       </div>
 
       <div>
-        <label for='remunerado'>Remunerado</label>
+        <label htmlFor='remunerado'>Remunerado</label>
         <input type='checkbox' name='remunerado' id='remunerado' checked={tarea.remunerado}
           onChange={eventoOnChange}/>
       </div>
 
       <div>
-        <label for='fecha'>Fecha</label>
+        <label htmlFor='fecha'>Fecha *</label>
         <input type='datetime-local' name='fecha' id='fecha' value={tarea.fecha} 
-          onChange={eventoOnChange}/>
+          onChange={eventoOnChange}/> &nbsp;
+        { creandoTarea && tarea.fecha.trim() === '' && <span className='error'>La fecha y hora es obligatorio</span>}
       </div>
 
       <div>
-        <label for='descripcion'>Descripción</label>
+        <label htmlFor='descripcion'>Descripción</label>
         <textarea rows='5' cols='30' name='descripcion' id='descripcion' 
           value={tarea.descripcion} onChange={eventoOnChange}></textarea>
       </div>
--- reactapp/src/epigrafe9/01ListaTareas.js
+++ reactapp/src/epigrafe9/01ListaTareas.js
@@ -1,30 +1,53 @@
 
 function ListaTareas({ tareas, setTareas}) {
+  const categorias = {
+    td: 'Tareas domésticas',
+    tc: 'Tareas comunitarias',
+    te: 'Tareas de la escuela',
+    oc: 'Ocio',
+    md: 'Mantenimiento digital'
+  };
+
+  const prioridad = {
+    b: 'Baja',
+    m: 'Media',
+    a: 'Alta',
+    u: 'Urgente'
+  };
+
+  function EliminarTarea(id) {
+    const pendientes = tareas.filter( (tarea) => tarea.id !== id );
+    setTareas(pendientes);
+  }
 
   return (
     <table>
       <thead>
-        <th>Id</th>
-        <th>Título</th>
-        <th>Categoría</th>
-        <th>Prioridad</th>
-        <th>Remunerado</th>
-        <th>Fecha</th>
-        <th>Descripcion</th>
+        <tr>
+          <th>Id</th>
+          <th>Título</th>
+          <th>Categoría</th>
+          <th>Prioridad</th>
+          <th>Remunerado</th>
+          <th>Fecha</th>
+          <th>Descripcion</th>
+          <th></th>
+        </tr>
       </thead>
       <tbody>
         {
-          tareas.map( (tarea) => {
+          tareas.map( (tarea) => 
             <tr key={tarea.id}>
               <td>{tarea.id}</td>
               <td>{tarea.titulo}</td>
-              <td>{tarea.categoria}</td>
-              <td>{tarea.prioridad}</td>
+              <td>{categorias[tarea.categoria]}</td>
+              <td>{prioridad[tarea.prioridad]}</td>
               <td>{tarea.remunerado ? "Si" : "Gratis"}</td>
-              <td>{tarea.fecha}</td>
+              <td>{tarea.fecha.toLocaleString()}</td>
               <td>{tarea.descripcion}</td>
+              <td><button onClick={() => EliminarTarea(tarea.id)}>Eliminar</button></td>
             </tr>
-          })
+          )
       }
       </tbody>
     </table>
